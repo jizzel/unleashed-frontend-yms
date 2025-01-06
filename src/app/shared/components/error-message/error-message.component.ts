6a4976f0ce438281d608661720428b9dcc6152cb
Joseph Afriyie Attakorh <juxluvjoe@gmail.com>
2025-01-06 14:15:06 +0000

    feat: design error message component

--- src/app/shared/components/error-message/error-message.component.ts
+++ src/app/shared/components/error-message/error-message.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-error-message',
   standalone: false,
-  
+
   templateUrl: './error-message.component.html',
   styleUrl: './error-message.component.css'
 })
 export class ErrorMessageComponent {
+  @Input() message = '';
+  @Input() type: 'error' | 'warning' | 'success' = 'error';
 
+  dismiss(): void {
+    this.message = '';
+  }
 }
