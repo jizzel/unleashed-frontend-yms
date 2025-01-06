afa91a606d74dcb80e721276f55938966cc0e52b
Joseph Afriyie Attakorh <juxluvjoe@gmail.com>
2025-01-06 14:15:22 +0000

    feat: design loader component

--- src/app/shared/components/loader/loader.component.ts
+++ src/app/shared/components/loader/loader.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-loader',
   standalone: false,
-  
+
   templateUrl: './loader.component.html',
   styleUrl: './loader.component.css'
 })
 export class LoaderComponent {
-
+  @Input() isLoading = false;
+  @Input() message?: string;
 }
